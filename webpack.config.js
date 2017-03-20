fee38c8f35b37e302ec744b047e04c3c8ffef4b4
du <951092973@qq.com>
2017-03-20 12:33:54 +0800

    区分打包环境

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,22 @@
 var webpack = require("webpack");
 var path = require("path");
 
+var output = {
+    path:path.resolve(__dirname,"dist"),
+    library:"ILoading",
+    libraryTarget:"umd",
+    publicPath:"/assets/"
+};
+var plugins = [
+    new webpack.ProvidePlugin({
+        $:"jquery"
+    })
+]
 var config = {
     entry:{
         app:"./src/main.js",
     },
-    output:{
-        filename:"iLoading.js",
-        path:path.resolve(__dirname,"dist"),
-        library:"ILoading",
-        libraryTarget:"umd",
-        publicPath:"/assets/"
-    },
     devtool:"source-map",
-    devServer:{
-        contentBase:path.resolve(__dirname,"./demo"),
-        port:9001,
-        compress:true,
-        noInfo:true,
-        hot:true
-    },
     resolve:{
         alias:{
             jquery$:path.resolve(__dirname,"lib/jquery.min.js")
@@ -40,13 +37,27 @@ var config = {
                 }
             }
         ]
-    },
-    plugins:[
-        new webpack.ProvidePlugin({
-            $:"jquery"
-        })
-    ]
+    }
 }
 
+var NODE_ENV = process.env.NODE_ENV || "development";
+
+
+if(NODE_ENV === "production"){
+    output.filename = "iLoading.min.js";
+    console.log("===========> production");
+}else{
+    output.filename = "iLoading.js";
+    config.devServer = {
+        contentBase:path.resolve(__dirname,"./demo"),
+        port:9001,
+        compress:true,
+        noInfo:true,
+        hot:true
+    }
+    console.log("===========> development");
+}
+config.output = output;
+config.plugins = plugins;
 
 module.exports = config;
